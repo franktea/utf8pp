4fb38f15045e093959de8aa4fc4bc757734f01ae
franktea <could.net@gmail.com>
2024-08-30 14:03:14 +0800

    bitset

--- test_main.cpp
+++ test_main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <bitset>
 #include <fmt/core.h>
 #include <range/v3/view/chunk_by.hpp>
 #include <range/v3/view/drop.hpp>
@@ -11,10 +12,13 @@ namespace vw = ranges::view;
 int main()
 {
     // utf8字节流
-    std::u8string str = u8"我是瓜王mel1，没瓜吃就喷人";
+    std::string str = //u8"我是瓜王mel1，没瓜吃就喷人";
+        // 下面这个是用python打印出来的
+        "\xe6\x88\x91\xe6\x98\xaf\xe7\x93\x9c\xe7\x8e\x8bmel1\xef\xbc\x8c\xe6\xb2\xa1\xe7\x93\x9c\xe5\x90\x83\xe5\xb0\xb1\xe5\x96\xb7\xe4\xba\xba";
     std::cout<<"字节数: "<<str.length()<<"\n";
     for(auto c: str) {
-        std::cout<< fmt::format("{0:08b}", char(c)) << " ";
+        std::cout<<std::bitset<8>(c)<<" ";
+        //std::cout<< fmt::format("{0:08b}", char(c)) << " ";
     }
     std::cout<<"\n\n";
 
@@ -33,7 +37,7 @@ int main()
     std::cout<<"\n\n";
 
     // 获取第N个字符
-    std::cout<<"第三个字符的字节串：";
+    std::cout<<"第三~四个字符的字节串：";
     {
         auto c3 = v | vw::drop(2) | vw::take(1);
         for(auto c: c3) {
@@ -43,16 +47,18 @@ int main()
         }
     }
     std::cout<<"\n";
-    std::cout<<"第三个字符的字符串：";
+    std::cout<<"第三~四个字符的字符串：";
     {
-        auto c3 = v | vw::drop(2) | vw::take(1);
+        auto c3 = v | vw::drop(2) | vw::take(2);
+        std::string s;
         for(auto c: c3) {
-            std::string s;
+            //std::string s;
             for(auto cc: c) {
                 s.push_back(cc);
             }
-            std::cout<<s;
+            //std::cout<<s;
         }
+        std::cout<<s<<"\n";
     }
     std::cout<<"\n";
 }
